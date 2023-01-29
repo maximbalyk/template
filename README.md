a5a770ba0b117ea35e17d0bbdd6fc7162b91a269
MaximBalykDev <maxim.balyk.dev@gmail.com>
2023-01-29 14:31:17 +0200

    First commit

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Base layout template with Gulp, SCSS and Stylelint
 Used [FIGMA DESIGN](https://www.figma.com/file/Ujp7bCFuvuJlkn8TSbQPSZ/%E2%84%9611-(kickstarter)?node-id=0%3A1)
-<!-- [DEMO LINK](https://maximbalyk.github.io/Kickstarter-) -->
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import './styles/main.scss';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Navigation/>
+      <Main/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
