6666ee87a3c05f847810d68847e2dac2012172a4
Maksym Balyk <maximbalyk@gmail.com>
2023-02-14 16:06:38 +0200

    add custom button to player

--- src/components/main/Presentation.js
+++ src/components/main/Presentation.js
@@ -1,21 +1,51 @@
-import React, {Component} from 'react';
+import React, { useRef, useState } from 'react';
 import intro from "../../video/intro.mp4";
 import aboutUsVideo from "../../images/about-usVideo.png";
+import playIco from "../../images/play.png"
+const Presentation = () => {
+    const [play, setPlay] = useState(false)
+    const [hover, setHover] = useState(false)
+    const videoRef = useRef()
+    const visibility = play ?
+        "video__button video__button--invisible"
+        : "video__button video__button--visible";
+    const handlePlay = () => {
+        if (!play) {
+            videoRef.current.play();
+            setPlay((play) => !play )
 
-class Presentation extends Component {
-    render() {
-        return (
-            <section className="presentation" id="presentation">
-                <video
-                    className="video video__full-width"
-                    src={intro}
-                    poster={aboutUsVideo}
-                    controls
-                >
-                </video>
-            </section>
-        );
+        } else {
+            videoRef.current.pause();
+            setPlay((play) => !play )
+        }
     }
+
+    return (
+        <section className="presentation" id="presentation">
+            <video
+                className="video video__full-width"
+                ref={videoRef}
+                poster={aboutUsVideo}
+            >
+                <source src={intro}/>
+            </video>
+            <button
+                className={visibility}
+                onClick={handlePlay}
+                onMouseEnter={()=>{setHover((hover)=> !hover)}}
+                onMouseLeave={()=>{setHover((hover)=> !hover)}}
+            >
+                {hover ? (
+                    <p>PLAY</p>
+                ):(
+                    <img
+                        src={playIco}
+                        alt="play"
+                    />)}
+            </button>
+
+        </section>
+    );
 }
 
-export default Presentation;
+export default Presentation
