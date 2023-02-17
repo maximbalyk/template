ef0cf60e04853bea54acc6110498365b9729cb1c
Maksym Balyk <maximbalyk@gmail.com>
2023-02-17 22:14:01 +0200

    fix warning in terminal

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,10 @@
 import React from "react";
 import logo from "../../images/logo.svg";
 import toTop from "../../images/to-top.svg";
-import {animateScroll as scroll } from "react-scroll";
+import { Link } from "react-scroll";
 
 export default class Footer extends React.Component {
     render() {
-        const scrollToTop = () => {
-            scroll.scrollToTop();
-        };
         return (
             <footer className="footer">
                 <div className="footer__logo">
@@ -19,24 +16,31 @@ export default class Footer extends React.Component {
                         />
                     </a>
 
-                    <a href="/" onClick={scrollToTop} className="footer__to-top">
+                    <Link className="footer__to-top" to="header">
                         <img src={toTop} alt="Back to top" />
-                    </a>
+                    </Link>
                 </div>
 
                 <ul className="footer__list">
                     <li className="footer__item">
-                        <a href="https://facebook.com/"
+                        <a
+                            href="https://facebook.com/"
                             className="footer__link footer__link--facebook"
                             target="_blank"
                             rel="noopener noreferrer"
-                        ></a>
+                        >
+
+                        </a>
                     </li>
                     <li className="footer__item">
-                        <a href="https://twitter.com/" className="footer__link footer__link--twitter"
+                        <a
+                            href="https://twitter.com/"
+                            className="footer__link footer__link--twitter"
                             target="_blank"
                             rel="noopener noreferrer"
-                        ></a>
+                        >
+
+                        </a>
                     </li>
                     <li className="footer__item">
                         <a
@@ -44,7 +48,9 @@ export default class Footer extends React.Component {
                             className="footer__link footer__link--instagram"
                             target="_blank"
                             rel="noopener noreferrer"
-                        ></a>
+                        >
+
+                        </a>
                     </li>
                 </ul>
                 <p className="footer__terms">
